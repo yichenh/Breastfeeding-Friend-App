b99a59b4c43048d9a4ffd933e900760a31ed8e55
yichenhao <yichen.hao@wustl.edu>
2018-11-14 10:36:20 -0600

    Video Embedded

---
+++ BFF2/BFF2/MainPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace BFF2
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        async void OnCallSampleVideo(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new sampleVideo());
+        }
+    }
+}
